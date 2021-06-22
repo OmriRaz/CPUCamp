54ef1bc619f1226870864eb1964b12029f2f651d
Omri Raz <43149084+OmriRaz@users.noreply.github.com>
2021-06-22 17:32:07 +0300

    Update ProgramCounter.v

--- CPU/ProgramCounter/ProgramCounter.v
+++ CPU/ProgramCounter/ProgramCounter.v
@@ -1,17 +1,17 @@
 
 module ProgramCounter(
-	input					  CLK_50,
-	input		[3:0]		SW,
-	input		[1:0]		BUTTON,
-	output	[6:0]		HEX0,
-	output	[6:0]		HEX1,
-	output	[6:0]		HEX2,
-	output	[7:0]		LED,
-	inout		[35:0]	GPIO
+	input			CLK_50,
+	input	[3:0]	SW,
+	input	[1:0]	BUTTON,
+	output	[6:0]	HEX0,
+	output	[6:0]	HEX1,
+	output	[6:0]	HEX2,
+	output	[7:0]	LED,
+	inout	[35:0]	GPIO
 );
 
 //=======================//
-//		PC reg / wires     //
+//    PC reg / wires     //
 //=======================//
 reg [3:0] Counter = 4'b0000; //4 bit program counter (change this value to jump)
 wire CE = 1'b1; //Count Enable (leave 1 as default)
@@ -20,7 +20,7 @@ assign CPUclk = CLK_50;
 
 
 //======================//
-//		 4 Bit counter    //
+//     4 Bit counter    //
 //======================//
 
 always @(posedge CPUclk & CE) begin
