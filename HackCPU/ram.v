90822250eeadf1466aba7698a0c9f73f8a74e206
Omri Raz <43149084+OmriRaz@users.noreply.github.com>
2021-06-27 02:09:35 +0300

    Update ram.v

--- HackCPU/ram.v
+++ HackCPU/ram.v
@@ -1,25 +1,19 @@
-
-
 module ram(
-	CPUclk,
-	addr,
-	rdata,
-	wdata,
-	we
+    CPUclk, //CPU clock input (default 50MHz)
+    addr,   //RAM address input
+    rdata,  //Read Data from RAM
+    wdata,  //Write Data to RAM
+    we      //Write Enable
 );
 input wire CPUclk, we;
 input wire [7:0] addr;
 input wire[15:0] wdata;
 output wire[15:0] rdata;
+reg[15:0] memory[0:255];
 assign rdata = memory[addr];
 
-reg[15:0] memory[0:255];
-
-
 always @(posedge CPUclk)
-
 if (we)
-
-memory[addr] <= wdata;
+    memory[addr] <= wdata;
 
 endmodule
