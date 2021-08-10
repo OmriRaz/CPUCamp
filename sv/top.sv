086542a78d39531f2868d1c2e164404fd2749f3d
Guy Shapira <gayuha@gmail.com>
2021-08-10 20:18:27 +0300

    Some refactoring. Fixed hex digits vertical display bug.

--- sv/top.sv
+++ sv/top.sv
@@ -8,20 +8,23 @@ module top(
         output      h_sync,
         output    v_sync
     );
-    parameter DATA_WIDTH = 16, RAM_REGISTER_COUNT = 2**9, RAM_SCREEN_OFFSET = 0;
-    parameter ROM_REGISTER_COUNT = 2**9;
+    parameter DATA_WIDTH = 16, RAM_REGISTER_COUNT = 2**12, RAM_SCREEN_OFFSET = 0;
+    parameter ROM_REGISTER_COUNT = 2**12;
 
-    parameter BITS_PER_MEMORY_PIXEL_X = 4;
-    parameter BITS_PER_MEMORY_PIXEL_Y = 5;
+    parameter BITS_PER_MEMORY_PIXEL_X = 4; //4
+    parameter BITS_PER_MEMORY_PIXEL_Y = 5; //5
     parameter HEX_START_X = 512;
-    parameter PIXELS_PER_HEX_DIGIT = 16;
+    parameter PIXELS_PER_HEX_DIGIT_X = 16;
+    parameter PIXELS_PER_HEX_DIGIT_Y = 32;
+
+    // The number of bits shown will be (2**(9-BITS_PER_MEMORY_PIXEL_X))*(384/(2**(BITS_PER_MEMORY_PIXEL_Y)))
 
     localparam HEX_DIGITS_PER_LINE = BITS_PER_MEMORY_PIXEL_X <= 4 ? 8 : 4;
     localparam WORDS_PER_LINE = (2**9) >> ($clog2(DATA_WIDTH)+BITS_PER_MEMORY_PIXEL_X);
     localparam PIXELS_PER_WORD = 2**($clog2(DATA_WIDTH)+BITS_PER_MEMORY_PIXEL_X);
     localparam BITS_PER_HEX_DIGIT = 4;
     localparam WORDS_PER_HEX_LINE = BITS_PER_HEX_DIGIT * HEX_DIGITS_PER_LINE / DATA_WIDTH;
-    localparam HEX_PIXELS_PER_WORD = DATA_WIDTH / BITS_PER_HEX_DIGIT * PIXELS_PER_HEX_DIGIT;
+    localparam HEX_PIXELS_PER_WORD = DATA_WIDTH / BITS_PER_HEX_DIGIT * PIXELS_PER_HEX_DIGIT_X;
 
     logic [$clog2(RAM_REGISTER_COUNT)-1:0] ram_address;
     logic we;
@@ -44,7 +47,7 @@ module top(
                                        + (pixel_x / PIXELS_PER_WORD));
         // HEX
         else
-            word_address = DATA_WIDTH'((pixel_y >> BITS_PER_MEMORY_PIXEL_Y) * WORDS_PER_HEX_LINE
+            word_address = DATA_WIDTH'((pixel_y / PIXELS_PER_HEX_DIGIT_Y) * WORDS_PER_HEX_LINE
                                        + ((pixel_x - HEX_START_X) / HEX_PIXELS_PER_WORD));
     end
 
@@ -61,7 +64,7 @@ module top(
                 );
 
     vga #(.DATA_WIDTH(DATA_WIDTH), .BITS_PER_MEMORY_PIXEL_X(BITS_PER_MEMORY_PIXEL_X), .BITS_PER_MEMORY_PIXEL_Y(BITS_PER_MEMORY_PIXEL_Y),
-          .HEX_START_X(HEX_START_X), .PIXELS_PER_HEX_DIGIT(PIXELS_PER_HEX_DIGIT))
+          .HEX_START_X(HEX_START_X), .PIXELS_PER_HEX_DIGIT_X(PIXELS_PER_HEX_DIGIT_X))
         vga_inst(.CLK_50(CLK_50),
                  .number_drawing_request(number_drawing_request),
                  .number_rgb(number_rgb),
@@ -85,17 +88,17 @@ module top(
     logic [7:0] current_nibble;
     always_comb
     begin
-        if ((pixel_x - HEX_START_X) % HEX_PIXELS_PER_WORD < PIXELS_PER_HEX_DIGIT*1)
+        if ((pixel_x - HEX_START_X) % HEX_PIXELS_PER_WORD < PIXELS_PER_HEX_DIGIT_X*1)
             current_nibble = word_value[15:12];
-        else if ((pixel_x - HEX_START_X) % HEX_PIXELS_PER_WORD < PIXELS_PER_HEX_DIGIT*2)
+        else if ((pixel_x - HEX_START_X) % HEX_PIXELS_PER_WORD < PIXELS_PER_HEX_DIGIT_X*2)
             current_nibble = word_value[11:8];
-        else if ((pixel_x - HEX_START_X) % HEX_PIXELS_PER_WORD < PIXELS_PER_HEX_DIGIT*3)
+        else if ((pixel_x - HEX_START_X) % HEX_PIXELS_PER_WORD < PIXELS_PER_HEX_DIGIT_X*3)
             current_nibble = word_value[7:4];
         else
             current_nibble = word_value[3:0];
     end
 
-    square_object #(.OBJECT_WIDTH_X(PIXELS_PER_HEX_DIGIT*HEX_DIGITS_PER_LINE), .OBJECT_HEIGHT_Y(32*12)) number_square(
+    square_object #(.OBJECT_WIDTH_X(PIXELS_PER_HEX_DIGIT_X*HEX_DIGITS_PER_LINE), .OBJECT_HEIGHT_Y(32*12)) number_square(
                       .pixelX(pixel_x),// current VGA pixel
                       .pixelY(pixel_y),
                       .topLeftX(HEX_START_X), //position on the screen
